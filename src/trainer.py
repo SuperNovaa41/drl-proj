3adce178d225711430b3e1b1c5c8735bd2a66366
Nathan Singer <nathan.r.singer@gmx.com>
2025-10-18 17:29:41 -0400

    makes the trainer save the model with the proper model name

--- src/trainer.py
+++ src/trainer.py
@@ -67,8 +67,9 @@ def main():
     model.set_logger(new_logger)
 
     model.learn(total_timesteps=args.timesteps, progress_bar=True)
+    
 
-    save_name = f"ppo_game_{args.reward_mode}"
+    save_name = f"{args.model_type}_game_{args.reward_mode}"
     path = os.path.join(args.modeldir, save_name)
     model.save(path)
     print(f"Saved model to {path}")
